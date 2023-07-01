ab85991651237508ad74e8231e8806c43588d133
g-dsservant <138117731+g-dsservant@users.noreply.github.com>
2023-06-30 22:54:40 -0700

    game.py

--- game.py
+++ game.py
@@ -1,68 +1,9 @@
 '''
 Game module
-Contains all necessary functions to run the game
+Contains all necessary functions for the game
 '''
 
 import os
-
-
-class CustomError(Exception):
-    '''
-    This class is used to raise exceptions in all user input functions in this file.
-
-    Attributes:
-          1) Message: The message associated an Exception
-    '''
-    def __init__(self,message):
-        '''
-        The constructor of CustomError class.
-
-            Parameters:
-                 Message: The text you want to be displayed when that particular Exception is raised.
-        '''
-        self.message = message
-
-
-def amount_check(chip,round_num=0):
-    '''
-    This function has two utilities:
-         1) To accept and validate total balance of the player at the start of the game from the user
-         2) To accept and validate the bet value at the start of every round from the user
-    '''
-    if not round_num:
-        while True:
-            try:
-                amount = int(input("Please enter your available balance: "))
-                if amount < 0:
-                    raise CustomError("Balance cannot be negative!")
-            
-            except CustomError as error:
-                print(f"{error.message}")
-            
-            except ValueError:
-                print("Enter a valid amount!")
-            
-            else:
-                chip.balance = amount
-                break
-    
-    else:
-        while True:
-            try:
-                amount = int(input(f"How much would you like to bet for Round {round_num}?: "))
-                if amount > chip.balance or amount <= 0:
-                    raise CustomError("Cannot bet outside your balance!")
-                
-            except ValueError:
-                print("Enter a valid amount!")
-        
-            except CustomError as error:
-                print(f"{error.message}")
-        
-            else:
-                chip.bet = amount
-                break
-
 
 def player_choice(hand,chip):
     '''
@@ -79,17 +20,17 @@ def player_choice(hand,chip):
                 choice = input("Do you want to Stand, Hit (S or H)?: ")
         
             if choice == 'DD' and (len(hand.cards) != 2 or chip.bet > (chip.balance/2)):
-                raise CustomError("You cannot Double Down at this point!")
-            
+                raise ValueError("You cannot Double Down at this point!")
+             
             if choice not in valid_options:
-                raise CustomError("Select a valid option!")
+                raise ValueError("Select a valid option!")
         
-        except CustomError as error:
-            print(f"{error.message}")
+        except ValueError as error:
+            print(error)
         
         else:
             return choice
-                
+
 
 def player_turn(hand_player,hand_dealer,chip,deck):
     '''
@@ -152,18 +93,14 @@ def dealer_turn(hand_player,hand_dealer,chip,deck):
 
     display_cards(hand_player,hand_dealer)
 
-    
     if hand_player.value() == 21 and len(hand_player.cards) == 2:
-        
         if hand_dealer.value() == 21:
             print("It's a push!")
         
         else:
             print(f"{hand_player.name} got a BlackJack!")
-            chip.add_balance((3/2)*chip.bet)
-        
+            chip.add_balance((3/2)*chip.bet)    
     else:
-        
         while hand_dealer.value() <= 16:
             print("Dealer has less than 17! Dealer gets to Hit!")
             hand_dealer.add_card(deck.deal())
@@ -172,14 +109,14 @@ def dealer_turn(hand_player,hand_dealer,chip,deck):
         if hand_dealer.value() > 21:
             print(f"Dealer busts, {hand_player.name} wins!")
             chip.add_balance(chip.bet)
-        
+            
         elif hand_dealer.value() > hand_player.value():
             print("Dealer has a higher value! Dealer wins!")
             chip.remove_balance(chip.bet)
         
         elif hand_dealer.value() == hand_player.value():
             print("It's a push!")
-        
+
         else:
             print(f"{hand_player.name} has a higher value! {hand_player.name} wins!")
             chip.add_balance(chip.bet)
@@ -193,12 +130,10 @@ def choice_to_continue():
     choice = 'wrong'
     
     while choice not in valid_options:
-        
         choice = input("Do you want to play another round (Y os N)?")
         
         if choice not in valid_options:
             print("Select a valid option!")
-        
         else:
             return choice == 'Y'
             
@@ -210,10 +145,9 @@ def display_cards(hand_player=0,hand_dealer=0):
     if hand_player:
         print("\n")
         print(f"{hand_player.name}'s cards ->", end = ' ')
-
+        
         for card in hand_player.cards:
             print(card,end=' ')
-
         print()
     
     if hand_dealer:
@@ -223,8 +157,7 @@ def display_cards(hand_player=0,hand_dealer=0):
             print(card,end=' ')
         
     print('\n')
-    
-        
+       
 
 if __name__ == '__main__':
     print("This is game.py module, please open blackjack.py")
